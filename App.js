fa890ab32bd93dddd86aaf0fe21f36d7c7ab6579
adehghan1376 <44525050+adehghan1376@users.noreply.github.com>
2019-02-22 20:30:14 +0330

    Update
    Some improvments to make it better like calculator

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
       {/*////////  1 Row  //////////////////*/}
         <View style={{flex:2,flexDirection:'row',padding:3}}>
         <View style={{flex:1}}>
-        <Text style={{backgroundColor:'#ffcfba',flex:1}}>0</Text>
+        <Text style={{backgroundColor:'#fcfdff',flex:1,paddingLeft:355,paddingTop:120,fontSize:50}}>0</Text>
         </View>        
         
         </View>
@@ -34,76 +34,77 @@ export default class App extends Component<Props> {
       {/*////////  2 Row  //////////////////*/}
         <View style={{flex:1,flexDirection:'row'}}>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>MC</Text>
+          <Text style={{backgroundColor:'#dee5f2',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>MC</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>MR</Text>
+          <Text style={{backgroundColor:'#dee5f2',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>MR</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>MS</Text>
+          <Text style={{backgroundColor:'#dee5f2',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>MS</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>M+</Text>
+          <Text style={{backgroundColor:'#dee5f2',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>M+</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>M-</Text>
+          <Text style={{backgroundColor:'#dee5f2',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>M-</Text>
           </View>
         </View>
       {/*////////  End of 2 Row  ///////////////////////////////////////*/} 
       {/*////////  3 Row  //////////////////*/}
         <View style={{flex:1,flexDirection:'row'}}>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>backspace</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:18}}>backspace</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>CE</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>CE</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>C</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>C</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>+-</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',paddingTop:20,fontSize:25}}>+</Text>
+            <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',paddingBottom:20,fontSize:40}}>-</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>SQRT</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>SQRT</Text>
           </View>
         </View>
       {/*////////  End of 3 Row  ///////////////////////////////////////*/} 
       {/*////////  4 Row  //////////////////*/}
       <View style={{flex:1,flexDirection:'row'}}>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>7</Text>
+          <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>7</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>8</Text>
+          <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>8</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>9</Text>
+          <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>9</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>/</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:25}}>/</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>%</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:25}}>%</Text>
           </View>
         </View>
       {/*////////  End of 4 Row  ///////////////////////////////////////*/} 
       {/*////////  5 Row  //////////////////*/}
       <View style={{flex:1,flexDirection:'row'}}>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>4</Text>
+          <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>4</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>5</Text>
+          <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>5</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>6</Text>
+          <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>6</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>*</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:25}}>*</Text>
           </View>
           <View style={{flex:1,flexDirection:'column',padding: 3}}>
-          <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>1/X</Text>
+          <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:20}}>1/X</Text>
           </View>
         </View>
       {/*////////  End of 5 Row  ///////////////////////////////////////*/} 
@@ -115,29 +116,29 @@ export default class App extends Component<Props> {
             <View style={{flex:1,flexDirection:'row'}}>
             
             <View style={{flex:1,flexDirection:'column',padding: 3}}>
-              <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>1</Text>
+              <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>1</Text>
             </View>
             <View style={{flex:1,flexDirection:'column',padding: 3}}>
-              <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>2</Text>
+              <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>2</Text>
             </View>
             <View style={{flex:1,flexDirection:'column',padding: 3}}>
-              <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>3</Text>
+              <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:30}}>3</Text>
             </View>
             <View style={{flex:1,flexDirection:'column',padding: 3}}>
-              <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center'}}>-</Text>
+              <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',fontSize:35}}>-</Text>
             </View>
             
             </View>
             {/*6 ended*/}
             <View style={{flex:1,flexDirection:'row'}}>
             <View style={{flex:2,flexDirection:'column'}}> 
-              <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center',margin:3}}>0</Text>
+              <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',margin:3,fontSize:40}}>0</Text>
             </View>
             <View style={{flex:1,flexDirection:'column'}}>
-              <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center',margin:3}}>.</Text>
+              <Text style={{backgroundColor:'#ffffff',flex:1,textAlign:'center',textAlignVertical:'center',margin:3,fontSize:30}}>.</Text>
             </View>
             <View style={{flex:1,flexDirection:'column'}}>
-              <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center',margin:3}}>+</Text>
+              <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',margin:3,fontSize:25}}>+</Text>
             </View>            
             </View>
 
@@ -145,7 +146,7 @@ export default class App extends Component<Props> {
           </View>
           {/*6&7 ended*/}
           <View style={{flex:1,flexDirection:'column',padding: 0}}>
-            <Text style={{backgroundColor:'#ffcfba',flex:1,textAlign:'center',textAlignVertical:'center',margin:3}}>=</Text>
+            <Text style={{backgroundColor:'#f2f6fc',flex:1,textAlign:'center',textAlignVertical:'center',margin:3,fontSize:50}}>=</Text>
           </View>
         </View>
       {/*////////  End of 6 & 7 Row  ///////////////////////////////////////*/} 
